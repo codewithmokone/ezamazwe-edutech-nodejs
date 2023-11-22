fc5ff47529a1990155e1fdd05a9a7d2b0943afe9
codewithmokone <codewithmokone@gmail.com>
2023-11-22 11:58:13 +0200

    updated

--- index.js
+++ index.js
@@ -295,14 +295,14 @@ app.post('/verify-email', [
   check('code').isEmail().notEmpty('Invalid code')
 ], async (req, res) => {
 
-  const errors = validationResult(req);
+  // const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json({ errors: errors.array() });
+  // }
 
   try {
-    
+
     const { code, email } = req.body;
 
     console.log("Code: ", code);
